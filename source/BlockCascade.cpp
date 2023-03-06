11c55f556a171dacfce101ee9fd21e6d0c18ea94
Daya Darsan <dayadarsan1802@gmail.com>
2023-03-06 15:43:41 +0530

    Add SFML, Create window

---
+++ source/BlockCascade.cpp
@@ -0,0 +1,25 @@
+﻿#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+	sf::RenderWindow window( sf::VideoMode { { 1280, 720 } }, {} );
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			switch (event.type )
+			{
+			case sf::Event::Closed:
+				window.close ();
+				return 0;
+			}
+		}
+			
+		window.clear ();
+		window.display ();
+	}
+	return 0;
+}
